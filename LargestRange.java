4c9556224891a915f37662da8c76d44e5a7d06ed
Suyeb Siddiqui <suyeb30391@gmail.com>
2019-10-29 14:39:08 +0100

    to get the  longest range in an array

---
+++ LargestRange.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+
+public class LargestRange {
+
+    public static int[] largestRange(int[] array) {
+        int[] bestRange = new int[2];
+        int longestlength = 0;
+        HashMap<Integer, Boolean> nums = new HashMap<>();
+        for(int num: array) {
+            nums.put(num, true);
+        }
+        for(int num : array) {
+            if(!nums.get(num)) {
+                continue;
+            }
+
+            nums.put(num, false);
+            int currentLength = 1;
+            int left = num-1;
+            int right = num+1;
+            while(nums.containsKey(left)) {
+                nums.put(left, false);
+                currentLength++;
+                left--;
+            }
+            while(nums.containsKey(right)) {
+                nums.put(right, false);
+                currentLength++;
+                right++;
+            }
+            if(currentLength> longestlength) {
+                longestlength = currentLength;
+                bestRange = new int[] {left + 1, right - 1};
+            }
+        }
+
+        return bestRange;
+    }
+
+    public static void main(String args[]) {
+
+        int[] sample = new int[] {8,4,2,10,3,6,7,9,1};
+        int[] result = largestRange(sample);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
