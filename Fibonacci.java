8f702cb7cb34382e81ecd731c70af828c0fb6c91
Suyeb Siddiqui <suyeb30391@gmail.com>
2019-10-29 14:12:20 +0100

    New Algo same code

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+    public static int getNthFib(int n) {
+        // Write your code here.
+        if (n == 2) {
+            return 1;
+        } else if (n == 1) {
+            return 0;
+        } else {
+            return getNthFib(n - 1) + getNthFib(n - 2);
+        }
+    }
+
+    public static void main (String args[]) {
+        System.out.println(getNthFib(45));
+    }
+}
